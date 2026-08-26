6d5a62cffb7cc64288b0ca451d0973c04bbe3b14
amorimtiago4-jpg <amorimtiago4@gmail.com>
2026-08-26 20:30:49 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,20 @@ import { useState, useEffect } from "react";
 const SUPA_URL = "https://vuhgcsraditjquklwoor.supabase.co";
 const SUPA_KEY = process.env.REACT_APP_SUPA_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ1aGdjc3JhZGl0anF1a2x3b29yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc3NDYzNjYsImV4cCI6MjEwMzMyMjM2Nn0.e-iW0KBTbSErJXHb0X6gDw-ZDrnV5ymE3Fui0QhUtSE";
 
+const ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ1aGdjc3JhZGl0anF1a2x3b29yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc3NDYzNjYsImV4cCI6MjEwMzMyMjM2Nn0.e-iW0KBTbSErJXHb0X6gDw-ZDrnV5ymE3Fui0QhUtSE";
+
 const supaFetch = async (path, options = {}) => {
+  const key = ANON_KEY;
+  const { prefer, headers: extraHeaders, ...restOptions } = options;
   const res = await fetch(SUPA_URL + "/rest/v1" + path, {
+    ...restOptions,
     headers: {
-      "apikey": SUPA_KEY,
-      "Authorization": "Bearer " + SUPA_KEY,
+      "apikey": key,
+      "Authorization": "Bearer " + key,
       "Content-Type": "application/json",
-      "Prefer": options.prefer || "return=representation",
-      ...options.headers,
+      "Prefer": prefer || "return=representation",
+      ...(extraHeaders || {}),
     },
-    ...options,
   });
   if (!res.ok) {
     const err = await res.text();
@@ -867,6 +871,3 @@ function AdminPanel({ adminCtx, users, logs, setUsersS, setLogsS, setScreen, sho
     </div>
   );
 }
-
-
-
